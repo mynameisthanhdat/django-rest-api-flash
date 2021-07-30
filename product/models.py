19e540cc13d99f6ed69621fcac945d9a8091fbe3
Datist <mynameisthanhdat@gmail.com>
2021-07-30 15:35:46 +0700

    Created model for product and migration

---
+++ product/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model) :
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    info = models.EmailField(max_length=10000)
+    price = models.IntegerField()
+    # date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) :
+        return self.name
