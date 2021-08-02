5f829dbc03c85b6141cddc8bed77567f859915aa
Datist <mynameisthanhdat@gmail.com>
2021-08-02 09:11:52 +0700

    created ViewSet and Router

--- product/urls.py
+++ product/urls.py
@@ -1,14 +1,47 @@
 from django.urls import path, include
-from .views import ProductList, ProductDetail, UserList, UserDetail, ProductHighlight
+# from .views import ProductList, ProductDetail, UserList, UserDetail, ProductHighlight
+from .views import UserViewSet, ProductViewSet, api_root
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework import renderers
+from rest_framework.routers import DefaultRouter
 
+# urlpatterns = [
+#     path('product/', ProductList.as_view(), name='product-list'),
+#     path('product/<int:pk>/', ProductDetail.as_view(), name='product-detail'),
+#     path('product/<int:pk>/highlight/', ProductHighlight.as_view(), name='product-highlight'),
+#     path('user/', UserList.as_view(), name='user-list'),
+#     path('user/<int:pk>/', UserDetail.as_view(), name='user-detail'),
+#     path('api-auth/', include('rest_framework.urls')),
+# ]
+
+product_list = ProductViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+product_detail = ProductViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+product_highlight = ProductViewSet.as_view({
+    'get': 'highlight'
+}, renderer_classes=[renderers.StaticHTMLRenderer])
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
+
+# urlpatterns = format_suffix_patterns(urlpatterns)
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'product', ProductViewSet)
+router.register(r'user', UserViewSet)
+
+# The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('product/', ProductList.as_view(), name='product-list'),
-    path('product/<int:pk>/', ProductDetail.as_view(), name='product-detail'),
-    path('product/<int:pk>/highlight/', ProductHighlight.as_view(), name='product-highlight'),
-    path('user/', UserList.as_view(), name='user-list'),
-    path('user/<int:pk>/', UserDetail.as_view(), name='user-detail'),
-    path('api-auth/', include('rest_framework.urls')),
+    path('', include(router.urls)),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
--- product/views.py
+++ product/views.py
@@ -12,48 +12,84 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework import renderers
+from rest_framework import viewsets
+from rest_framework.decorators import action
 
-class ProductList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+# ==========> start region APIView <==========
+
+# class ProductList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+
+#     def perform_create(self, serializer):
+#         serializer.save(owner=self.request.user)
+
+
+# class ProductDetail(mixins.RetrieveModelMixin,
+#                     mixins.UpdateModelMixin,
+#                     mixins.DestroyModelMixin,
+#                     generics.GenericAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+#                       IsOwnerOrReadOnly]
+
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+
+#     def put(self, request, *args, **kwargs):
+#         return self.update(request, *args, **kwargs)
+
+#     def delete(self, request, *args, **kwargs):
+#         return self.destroy(request, *args, **kwargs)
+
+# class ProductHighlight(generics.GenericAPIView):
+#     queryset = Product.objects.all()
+#     renderer_classes = [renderers.StaticHTMLRenderer]
+
+#     def get(self, request, *args, **kwargs):
+#         product = self.get_object()
+#         return Response(product.highlighted)
+
+# class UserList(generics.ListAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+
+
+# class UserDetail(generics.RetrieveAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+
+# ==========> end region APIView <==========
+
+# ==========> start region ViewSet <==========
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        product = self.get_object()
+        return Response(product.highlighted)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-
-class ProductDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
-                      IsOwnerOrReadOnly]
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+# ==========> start region ViewSet <==========
 
 @api_view(['GET'])
 def api_root(request, format=None):
@@ -61,11 +97,3 @@ def api_root(request, format=None):
         'user': reverse('user-list', request=request, format=format),
         'product': reverse('product-list', request=request, format=format)
     })
-
-class ProductHighlight(generics.GenericAPIView):
-    queryset = Product.objects.all()
-    renderer_classes = [renderers.StaticHTMLRenderer]
-
-    def get(self, request, *args, **kwargs):
-        product = self.get_object()
-        return Response(product.highlighted)
